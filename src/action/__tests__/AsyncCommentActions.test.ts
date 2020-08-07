acb0ee3a1c9cd837ed46d23916cda020fe737fa8
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-08-07 13:46:05 +0200

    [Task #1489] Merging comments module from KODI.
    (cherry picked from commit 3be6b3583c925514bcd239123aa7888fa5004bea)

---
+++ src/action/__tests__/AsyncCommentActions.test.ts
@@ -0,0 +1,86 @@
+import configureMockStore, {MockStoreEnhanced} from "redux-mock-store";
+import TermItState from "../../model/TermItState";
+import thunk from "redux-thunk";
+import VocabularyUtils, {IRI} from "../../util/VocabularyUtils";
+import Generator from "../../__tests__/environment/Generator";
+import Ajax from "../../util/Ajax";
+import {ThunkDispatch} from "../../util/Types";
+import {loadTermComments} from "../AsyncCommentActions";
+import ActionType from "../ActionType";
+import AsyncActionStatus from "../AsyncActionStatus";
+import Comment from "../../model/Comment";
+
+jest.mock("../../util/Routing");
+jest.mock("../../util/Ajax", () => {
+    const originalModule = jest.requireActual("../../util/Ajax");
+    return {
+        ...originalModule,
+        default: jest.fn()
+    };
+});
+
+const COMMENT_TYPE = "http://rdfs.org/sioc/types#Comment";
+
+const mockStore = configureMockStore<TermItState>([thunk]);
+
+describe("AsyncCommentActions", () => {
+    let store: MockStoreEnhanced<TermItState>;
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        store = mockStore(new TermItState());
+    });
+
+    describe("loadTermComments", () => {
+        it("sends term loading request for specified term identifier", () => {
+            const termIri = VocabularyUtils.create(Generator.generateUri());
+            const comments = generateComments(termIri);
+            Ajax.get = jest.fn().mockResolvedValue(comments);
+            return Promise.resolve((store.dispatch as ThunkDispatch)(loadTermComments(termIri))).then(() => {
+                expect(Ajax.get).toHaveBeenCalled();
+                const args = (Ajax.get as jest.Mock).mock.calls[0];
+                expect(args[0]).toContain(`${termIri.fragment}/comments`);
+                expect(args[1].getParams().namespace).toEqual(termIri.namespace);
+                expect(store.getActions().find(a => a.type === ActionType.LOAD_COMMENTS && a.status === AsyncActionStatus.REQUEST)).toBeDefined();
+            });
+        });
+
+        function generateComments(termIri: IRI) {
+            const comments = [];
+            for (let i = 0; i < 5; i++) {
+                comments.push({
+                    "@id": Generator.generateUri(),
+                    "@type": [COMMENT_TYPE],
+                    "http://rdfs.org/sioc/ns#content": "Test one",
+                    "http://rdfs.org/sioc/ns#has_creator": require("../../rest-mock/current.json"),
+                    "http://rdfs.org/sioc/ns#topic": {
+                        "@id": termIri.toString()
+                    }
+                });
+            }
+            return comments;
+        }
+
+        it("returns comments extracted from response JSON-LD", () => {
+            const termIri = VocabularyUtils.create(Generator.generateUri());
+            const comments = generateComments(termIri);
+            Ajax.get = jest.fn().mockResolvedValue(comments);
+            return Promise.resolve((store.dispatch as ThunkDispatch)(loadTermComments(termIri))).then((result: Comment[]) => {
+                expect(Ajax.get).toHaveBeenCalled();
+                expect(result.length).toEqual(comments.length);
+                expect(result.map(c => c.iri)).toEqual(comments.map(c => c["@id"]));
+                result.forEach(c => expect(c).toBeInstanceOf(Comment));
+            });
+        });
+
+        it("returns empty array on request failure", () => {
+            const termIri = VocabularyUtils.create(Generator.generateUri());
+            Ajax.get = jest.fn().mockRejectedValue({status: 404, error: {message: "Term not found"}});
+            return Promise.resolve((store.dispatch as ThunkDispatch)(loadTermComments(termIri))).then((result: Comment[]) => {
+                expect(Ajax.get).toHaveBeenCalled();
+                expect(result).toBeDefined();
+                expect(result.length).toEqual(0);
+            });
+        });
+    });
+});
