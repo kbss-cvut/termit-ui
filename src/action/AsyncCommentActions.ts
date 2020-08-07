a015d085954e832029b1654b30b96cdbc5469a87
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-08-07 13:46:05 +0200

    [Task #1489] Merging comments module from KODI.
    (cherry picked from commit 3be6b3583c925514bcd239123aa7888fa5004bea)

---
+++ src/action/AsyncCommentActions.ts
@@ -0,0 +1,25 @@
+import {IRI} from "../util/VocabularyUtils";
+import ActionType from "./ActionType";
+import {ThunkDispatch} from "../util/Types";
+import {asyncActionFailure, asyncActionRequest, asyncActionSuccess} from "./SyncActions";
+import Ajax, {param} from "../util/Ajax";
+import Constants from "../util/Constants";
+import JsonLdUtils from "../util/JsonLdUtils";
+import Comment, {CommentData, CONTEXT as COMMENT_CONTEXT} from "../model/Comment";
+import {ErrorData} from "../model/ErrorInfo";
+
+export function loadTermComments(termIri: IRI) {
+    const action = {type: ActionType.LOAD_COMMENTS};
+    return (dispatch: ThunkDispatch) => {
+        dispatch(asyncActionRequest(action, true));
+        return Ajax.get(`${Constants.API_PREFIX}/terms/${termIri.fragment}/comments`, param("namespace", termIri.namespace))
+            .then((data: object) => JsonLdUtils.compactAndResolveReferencesAsArray<CommentData>(data, COMMENT_CONTEXT))
+            .then((data: CommentData[]) => {
+                dispatch(asyncActionSuccess(action));
+                return data.map(d => new Comment(d));
+            }).catch((error: ErrorData) => {
+                dispatch(asyncActionFailure(action, error));
+                return [];
+            });
+    }
+}
