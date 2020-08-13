a67f5c62a444cb3cf20ab4f1bf77162327358b43
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-08-13 14:52:41 +0200

    [Task #1489] Merging comments module from KODI.
    (cherry picked from commit 1607a5ffdb3a094668622b0bc50c72c6b61862a8)

--- src/component/comment/CommentView.tsx
+++ src/component/comment/CommentView.tsx
@@ -2,47 +2,34 @@ import * as React from "react";
 import withI18n, {HasI18n} from "../hoc/withI18n";
 import Comment from "../../model/Comment";
 import {injectIntl} from "react-intl";
-import {FaUserCircle} from "react-icons/fa";
+import {FaUserCircle, FaRegThumbsUp, FaRegThumbsDown, FaThumbsUp, FaThumbsDown} from "react-icons/fa";
 import TimeAgo from "javascript-time-ago";
 import Utils from "../../util/Utils";
-import CommentLikes from "./CommentLikes";
-import CommentDislikes from "./CommentDislikes";
-import User from "../../model/User";
-import TermItState from "../../model/TermItState";
-import {connect} from "react-redux";
+import CommentReactions from "./CommentReactions";
 
 interface CommentViewProps extends HasI18n {
     comment: Comment;
-    addReaction: (comment: Comment, reactionType: string) => void;
-    removeReaction: (comment: Comment) => void;
-    onEdit: (comment: Comment) => void;
-
-    currentUser: User;
 }
 
 const CommentView: React.FC<CommentViewProps> = props => {
-    const {comment, addReaction, removeReaction, onEdit, currentUser} = props;
+    const {comment, i18n} = props;
     const formatter = new TimeAgo(props.locale);
-    const canEdit = comment.author!.iri === currentUser.iri;
     return <div className="comment mt-2 pt-2">
         <div className="float-left avatar"><FaUserCircle/></div>
         <div className="content">
             <span className="author">{comment.author!.fullName}</span>
             <div className="metadata text-muted">
                 <div className="mr-2 d-inline-block">{formatter.format(comment.created!)}</div>
-                <CommentLikes comment={comment} reactions={Utils.sanitizeArray(comment.reactions)}
-                              addReaction={addReaction} removeReaction={removeReaction}/>
-                <CommentDislikes comment={comment} reactions={Utils.sanitizeArray(comment.reactions)}
-                                 addReaction={addReaction} removeReaction={removeReaction}/>
-                {comment.modified && <div className="ml-3 d-inline-block italics">{props.i18n("comments.comment.edited")}</div>}
+                <CommentReactions reactions={Utils.sanitizeArray(comment.likes)} iconOn={FaThumbsUp}
+                                  iconOff={FaRegThumbsUp} iconOffTitle={i18n("comments.comment.like")}
+                                  iconOnTitle={"comments.comment.like.on"}/>
+                <CommentReactions reactions={Utils.sanitizeArray(comment.dislikes)} iconOn={FaThumbsDown}
+                                  iconOff={FaRegThumbsDown} iconOffTitle={i18n("comments.comment.dislike")}
+                                  iconOnTitle={"comments.comment.dislike.on"}/>
             </div>
-            <div className="mt-1 mb-2 comment-text">{comment.content}</div>
-            <div className="actions">
-                {canEdit &&
-                <span className="comment-action btn-outline-primary" onClick={() => onEdit(comment)}>{props.i18n("edit")}</span>}
-            </div>
+            <div className="mt-1 mb-2">{comment.content}</div>
         </div>
     </div>;
 };
 
-export default connect((state: TermItState) => ({currentUser: state.user}))(injectIntl(withI18n(CommentView)));
+export default injectIntl(withI18n(CommentView));
