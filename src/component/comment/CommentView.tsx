0037a392649b47066ec46f18a43ed9480f394955
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-08-13 18:43:02 +0200

    [Task #1489] Merging comments module from KODI.
    (cherry picked from commit 4aef0f2499667262fbc38bf0f13c1b6ce2b4a8a4)

--- src/component/comment/CommentView.tsx
+++ src/component/comment/CommentView.tsx
@@ -2,17 +2,18 @@ import * as React from "react";
 import withI18n, {HasI18n} from "../hoc/withI18n";
 import Comment from "../../model/Comment";
 import {injectIntl} from "react-intl";
-import {FaUserCircle, FaRegThumbsUp, FaRegThumbsDown, FaThumbsUp, FaThumbsDown} from "react-icons/fa";
+import {FaUserCircle} from "react-icons/fa";
 import TimeAgo from "javascript-time-ago";
 import Utils from "../../util/Utils";
-import CommentReactions from "./CommentReactions";
+import CommentLikes from "./CommentLikes";
+import CommentDislikes from "./CommentDislikes";
 
 interface CommentViewProps extends HasI18n {
     comment: Comment;
 }
 
 const CommentView: React.FC<CommentViewProps> = props => {
-    const {comment, i18n} = props;
+    const {comment} = props;
     const formatter = new TimeAgo(props.locale);
     return <div className="comment mt-2 pt-2">
         <div className="float-left avatar"><FaUserCircle/></div>
@@ -20,12 +21,8 @@ const CommentView: React.FC<CommentViewProps> = props => {
             <span className="author">{comment.author!.fullName}</span>
             <div className="metadata text-muted">
                 <div className="mr-2 d-inline-block">{formatter.format(comment.created!)}</div>
-                <CommentReactions reactions={Utils.sanitizeArray(comment.likes)} iconOn={FaThumbsUp}
-                                  iconOff={FaRegThumbsUp} iconOffTitle={i18n("comments.comment.like")}
-                                  iconOnTitle={"comments.comment.like.on"}/>
-                <CommentReactions reactions={Utils.sanitizeArray(comment.dislikes)} iconOn={FaThumbsDown}
-                                  iconOff={FaRegThumbsDown} iconOffTitle={i18n("comments.comment.dislike")}
-                                  iconOnTitle={"comments.comment.dislike.on"}/>
+                <CommentLikes comment={comment} likes={Utils.sanitizeArray(comment.likes)}/>
+                <CommentDislikes comment={comment} dislikes={Utils.sanitizeArray(comment.dislikes)}/>
             </div>
             <div className="mt-1 mb-2">{comment.content}</div>
         </div>
