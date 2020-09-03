e04cd93f34769a5dd82c6155fde5c9bdee4f1553
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-09-03 12:50:15 +0200

    [Enhancement #6] Implement comment editing.
    (cherry picked from commit 49d6ea101043f97d60ff5b7a96a90fdf10bc6433)

--- src/component/comment/CommentEdit.tsx
+++ src/component/comment/CommentEdit.tsx
@@ -3,6 +3,8 @@ import Comment from "../../model/Comment";
 import withI18n, {HasI18n} from "../hoc/withI18n";
 import {injectIntl} from "react-intl";
 import TextArea from "../misc/TextArea";
+import {Button, ButtonToolbar} from "reactstrap";
+import {FaUserCircle} from "react-icons/fa";
 
 interface CommentEditProps extends HasI18n {
     comment: Comment;
@@ -10,12 +12,33 @@ interface CommentEditProps extends HasI18n {
     onCancel: () => void;
 }
 
-const CommentEdit:React.FC<CommentEditProps> = props => {
-    const {comment} = props;
+const CommentEdit: React.FC<CommentEditProps> = props => {
+    const {comment, onCancel, onSubmit, i18n} = props;
     const [content, setContent] = React.useState(comment.content);
+    const submit = () => {
+        onSubmit(new Comment(Object.assign({}, comment, {content})));
+    };
+    const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Escape") {
+            onCancel();
+        } else if (e.key === "Enter" && !e.shiftKey && !e.ctrlKey && content.trim().length > 0) {
+            e.preventDefault();
+            submit();
+        }
+    };
 
-    // TODO
-    return <TextArea value={content} onChange={e => setContent(e.target.value)}/>;
+    return <div className="comment mt-2 pt-2">
+        <div className="float-left avatar"><FaUserCircle/></div>
+        <div className="content">
+            <TextArea value={content} autoFocus={true} onChange={e => setContent(e.target.value)} onKeyDown={onKeyDown}/>
+            <ButtonToolbar className="justify-content-end">
+                <Button color="success" id="comment-edit-submit" size="sm" disabled={content.trim().length === 0}
+                        onClick={() => props.onSubmit(comment)}>{i18n("save")}</Button>
+                <Button color="outline-dark" id="comment-edit-cancel" size="sm"
+                        onClick={onCancel}>{i18n("cancel")}</Button>
+            </ButtonToolbar>
+        </div>
+    </div>;
 };
 
 export default injectIntl(withI18n(CommentEdit));
