cbc9e9c9b455ec947b896103ce94f90cc0f70362
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-09-15 08:28:37 +0200

    [Bug #5] Update i18n translation.

--- src/i18n/cs.ts
+++ src/i18n/cs.ts
@@ -522,7 +522,7 @@ export default {
         "workspace.select.success": "Pracovní prostor \"{name}\" úspěšně načten.",
         "workspace.loading": "Načítám aktuální pracovní prostor...",
         "workspace.current.empty": "Aktuálně nemáte načtený žádný pracovní prostor.",
-        "workspace.indicator": "Prac. prostor: {name}",
+        "workspace.indicator": "Pracovní prostor: {name}",
 
         "comments.title": "Komentáře",
         "comments.create.placeholder": "Napište komentář...",
