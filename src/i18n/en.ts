180614bfdc7ee2a0cd2217bedb1173461f1ff2d4
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-10-20 17:29:13 +0200

    [Enhancement #15] Add EU funding info (logo with link).
    Also fix incorrect project number in Readme.

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -25,6 +25,7 @@ export default {
 
         'footer.copyright': 'KBSS at FEE CTU in Prague',
         'footer.version': 'Version',
+        "footer.grant.text": "This application is developed as part of the project OPZ no. CZ.03.4.74/0.0/0.0/15_025/0013983.",
 
         'news-viewer.title': "News",
 
