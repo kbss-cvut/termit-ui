8e1c54d4bcf18fa4ce03c486cef3b103a8add409
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-11-05 15:48:24 +0100

    [Task #1431] Introduce basic router-level code splitting.

--- src/component/resource/ResourceRoute.tsx
+++ src/component/resource/ResourceRoute.tsx
@@ -5,15 +5,19 @@ import withI18n, {HasI18n} from "../hoc/withI18n";
 import {injectIntl} from "react-intl";
 import ResourceSummaryRoute from "./ResourceSummaryRoute";
 import BreadcrumbRoute from "../breadcrumb/BreadcrumbRoute";
-import ResourceFileDetail from "./ResourceFileDetail";
+import Mask from "../misc/Mask";
+
+const ResourceFileDetail = React.lazy(() => import("./ResourceFileDetail"));
 
 const ResourceRoute: React.FC<HasI18n & RouteComponentProps<any>> = (props) => {
-    return <Switch>
-        <BreadcrumbRoute title={props.i18n("annotator.annotate-content")} path={Routes.annotateFile.path}
-                         component={ResourceFileDetail}/>
-        <Route asset="resource" path={Routes.resourceSummary.path}
-                                includeSearch={true} component={ResourceSummaryRoute}/>
-    </Switch>;
+    return <React.Suspense fallback={<Mask/>}>
+        <Switch>
+            <BreadcrumbRoute title={props.i18n("annotator.annotate-content")} path={Routes.annotateFile.path}
+                             component={ResourceFileDetail}/>
+            <Route asset="resource" path={Routes.resourceSummary.path}
+                   includeSearch={true} component={ResourceSummaryRoute}/>
+        </Switch>
+    </React.Suspense>;
 };
 
 export default injectIntl(withI18n(ResourceRoute));
