04ef6135635ada580b274a1f7cb16cad111cfa5a
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-11-10 10:58:31 +0100

    [Bug #1441] Show localized term link in Annotator.
    Ensure TermLink creates a defensive copy of the term when manipulating its label.

--- src/component/term/__tests__/TermLink.test.tsx
+++ src/component/term/__tests__/TermLink.test.tsx
@@ -7,21 +7,28 @@ import Vocabulary from "../../../model/Vocabulary";
 import Generator from "../../../__tests__/environment/Generator";
 import {EMPTY_USER} from "../../../model/User";
 import {intlFunctions} from "../../../__tests__/environment/IntlUtil";
+import MultilingualString, {langString} from "../../../model/MultilingualString";
+import Constants from "../../../util/Constants";
 
 describe("TermLink", () => {
 
-    it("links to correct internal asset", () => {
-        const vocFragment = "localVocabularyFragment";
-        const vocNamespace = "http://test.org/";
-        const vocabulary = new Vocabulary({
+    const vocFragment = "localVocabularyFragment";
+    const vocNamespace = "http://test.org/";
+    let testVocabulary: Vocabulary;
+
+    beforeEach(() => {
+        testVocabulary = new Vocabulary({
             label: "Test vocabulary",
             iri: vocNamespace + vocFragment
         });
+    })
+
+    it("links to correct internal asset", () => {
         const termFragment = "localTermFragment";
         const term = new Term({
-            label: "Test term",
-            iri: `${vocabulary.iri}/pojem/${termFragment}`,
-            vocabulary
+            label: langString("Test term"),
+            iri: `${testVocabulary.iri}/pojem/${termFragment}`,
+            vocabulary: testVocabulary
         });
 
         const link = mountWithIntl(<MemoryRouter><TermLink term={term}
@@ -32,7 +39,7 @@ describe("TermLink", () => {
     it("render outgoing link when term is missing vocabulary", () => {
         const term = new Term({
             iri: Generator.generateUri(),
-            label: "Term without vocabulary"
+            label: langString("Term without vocabulary")
         });
         const wrapper = mountWithIntl(<MemoryRouter><TermLink term={term}
                                                               user={Generator.generateUser()} {...intlFunctions()}/></MemoryRouter>);
@@ -43,21 +50,48 @@ describe("TermLink", () => {
     });
 
     it("links to public term view when user is not logged in", () => {
-        const vocFragment = "localVocabularyFragment";
-        const vocNamespace = "http://test.org/";
-        const vocabulary = new Vocabulary({
-            label: "Test vocabulary",
-            iri: vocNamespace + vocFragment
-        });
         const termFragment = "localTermFragment";
         const term = new Term({
-            label: "Test term",
-            iri: `${vocabulary.iri}/pojem/${termFragment}`,
-            vocabulary
+            label: langString("Test term"),
+            iri: `${testVocabulary.iri}/pojem/${termFragment}`,
+            vocabulary: testVocabulary
         });
 
         const link = mountWithIntl(<MemoryRouter><TermLink term={term}
                                                            user={EMPTY_USER} {...intlFunctions()}/></MemoryRouter>).find(Link);
         expect((link.props() as any).to).toEqual(`/public/vocabularies/${vocFragment}/terms/${termFragment}?namespace=${vocNamespace}`);
-    })
+    });
+
+    it("does not change term in props when dealing with multilingual labels", () => {
+        const originalLabel: MultilingualString = {
+            "en": "Test term",
+            "cs": "Testovaci pojem"
+        };
+        const term = new Term({
+            label: originalLabel,
+            iri: `${testVocabulary.iri}/pojem/localTestFragment`,
+            vocabulary: testVocabulary
+        });
+
+        mountWithIntl(<MemoryRouter><TermLink term={term}
+                                              user={EMPTY_USER} {...intlFunctions()}/></MemoryRouter>).find(Link);
+        expect(term.label).toEqual(originalLabel);
+    });
+
+    it("renders link with label in language corresponding to current locale", () => {
+        const term = new Term({
+            label: {
+                "en": "Test term",
+                "cs": "Testovaci pojem"
+            },
+            iri: `${testVocabulary.iri}/pojem/localTestFragment`,
+            vocabulary: testVocabulary
+        });
+
+        const intlData = intlFunctions();
+        intlData.locale = Constants.LANG.CS.locale;
+        const link = mountWithIntl(<MemoryRouter><TermLink term={term}
+                                                           user={EMPTY_USER} {...intlData}/></MemoryRouter>).find(Link);
+        expect(link.text()).toEqual(term.label.cs);
+    });
 });
