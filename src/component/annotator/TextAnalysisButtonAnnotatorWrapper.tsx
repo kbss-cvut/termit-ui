f26f733352835b723e65e493bb800559851b04ee
Petr Křemen <petr.kremen@gmail.com>
2020-11-12 11:48:00 +0100

    [R1442] make analysis button depend on the default vocabulary provided.

--- src/component/annotator/TextAnalysisButtonAnnotatorWrapper.tsx
+++ src/component/annotator/TextAnalysisButtonAnnotatorWrapper.tsx
@@ -1,26 +1,16 @@
 import * as React from "react";
 import {IRI} from "../../util/VocabularyUtils";
-import {connect} from "react-redux";
-import TermItState from "../../model/TermItState";
 import TextAnalysisInvocationButton from "../resource/file/TextAnalysisInvocationButton";
-import Vocabulary from "../../model/Vocabulary";
 
 interface ShowTextAnalysisInvocationButtonProps {
     fileIri: IRI;
     vocabularyIri: IRI;
-    vocabulary?: Vocabulary;
 }
 
-const ShowTextAnalysisInvocationButton: React.FC<ShowTextAnalysisInvocationButtonProps> = props => {
-    const showTextAnalysisButton = props.vocabulary && props.vocabulary!.document && props.vocabulary!.document.files;
-    return showTextAnalysisButton ?
-        <TextAnalysisInvocationButton
-            className="analyze-button"
-            file={props.vocabulary!.document!.files.find(f => f.iri === props.fileIri.toString())!}
-            vocabularyIri={props.vocabulary!.iri.toString()}/>
-        : null;
-}
+const ShowTextAnalysisInvocationButton: React.FC<ShowTextAnalysisInvocationButtonProps> = props =>
+    <TextAnalysisInvocationButton
+        className="analyze-button"
+        fileIri={props.fileIri}
+        defaultVocabularyIri={props.vocabularyIri.toString()}/>;
 
-export default connect((state: TermItState) => ({
-    vocabulary: state.vocabulary
-}))(ShowTextAnalysisInvocationButton);
+export default ShowTextAnalysisInvocationButton;
