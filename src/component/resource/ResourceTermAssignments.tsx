de32b5c83ee41b0ca6087299476874c75e00ce00
Petr Křemen <petr.kremen@gmail.com>
2020-11-19 16:04:09 +0100

    [R1418] removing occurrences from resource detail view

--- src/component/resource/ResourceTermAssignments.tsx
+++ src/component/resource/ResourceTermAssignments.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import {injectIntl} from "react-intl";
-import classNames from "classnames";
 import withI18n, {HasI18n} from "../hoc/withI18n";
 import Resource, {EMPTY_RESOURCE} from "../../model/Resource";
 import {connect} from "react-redux";
@@ -8,11 +7,10 @@ import {ThunkDispatch} from "../../util/Types";
 import {loadResourceTermAssignmentsInfo} from "../../action/AsyncActions";
 import VocabularyUtils from "../../util/VocabularyUtils";
 import Term from "../../model/Term";
-import {Badge, Col, Label, Row} from "reactstrap";
+import {Col, Label, Row} from "reactstrap";
 import TermLink from "../term/TermLink";
 import {
     ResourceTermAssignments as TermAssignmentInfo,
-    ResourceTermOccurrences
 } from "../../model/ResourceTermAssignments";
 import Utils from "../../util/Utils";
 import AppNotification from "../../model/AppNotification";
@@ -48,10 +46,6 @@ function isOccurrence(item: TermAssignmentInfo) {
     return Utils.sanitizeArray(item.types).indexOf(VocabularyUtils.TERM_OCCURRENCE) !== -1;
 }
 
-function isFile(resource: Resource) {
-    return Utils.getPrimaryAssetType(resource) === VocabularyUtils.FILE;
-}
-
 export class ResourceTermAssignments extends React.Component<ResourceTermAssignmentsProps, ResourceTermAssignmentsState> {
     constructor(props: ResourceTermAssignmentsProps) {
         super(props);
@@ -84,7 +78,6 @@ export class ResourceTermAssignments extends React.Component<ResourceTermAssignm
     public render() {
         const i18n = this.props.i18n;
         const assignments = this.renderAssignedTerms();
-        const occurrencesClass = classNames("mt-3", {"m-resource-term-occurrences-container": assignments.length > 0});
         return <>
             <Row>
                 <Col xl={2} md={4}>
@@ -96,16 +89,6 @@ export class ResourceTermAssignments extends React.Component<ResourceTermAssignm
                     {assignments}
                 </Col>
             </Row>
-            {isFile(this.props.resource) && <Row className={occurrencesClass}>
-                <Col xl={2} md={4}>
-                    <Label className="attribute-label" title={i18n("resource.metadata.terms.occurrences.tooltip")}>
-                        {i18n("resource.metadata.terms.occurrences")}:
-                    </Label>
-                </Col>
-                <Col xl={10} md={8} id="resource-metadata-term-occurrences" className="resource-terms">
-                    {this.renderTermOccurrences()}
-                </Col>
-            </Row>}
         </>;
     }
 
@@ -120,38 +103,6 @@ export class ResourceTermAssignments extends React.Component<ResourceTermAssignm
                                     vocabulary: rta.vocabulary,
                                     draft: rta.term.draft
                                 })}/>
-                        </span>);
-        });
-        return items;
-    }
-
-    private renderTermOccurrences() {
-        const items: JSX.Element[] = [];
-        const occurrences = new Map<string, { term: Term, suggestedCount: number}>();
-        this.state.assignments.filter(isOccurrence).forEach(rta => {
-            if (!occurrences.has(rta.term.iri!)) {
-                occurrences.set(rta.term.iri!, {
-                    term: new Term({
-                        iri: rta.term.iri,
-                        label: langString(rta.label),
-                        vocabulary: rta.vocabulary,
-                        draft: rta.term.draft
-                    }),
-                    suggestedCount: 0
-                });
-            }
-            if (Utils.sanitizeArray(rta.types).indexOf(VocabularyUtils.SUGGESTED_TERM_OCCURRENCE) !== -1) {
-                occurrences.get(rta.term.iri!)!.suggestedCount = (rta as ResourceTermOccurrences).count;
-            }
-        });
-        occurrences
-            .forEach((v, k) => {
-                if ( v.suggestedCount > 0 )
-            items.push(<span key={k} className="resource-term-link m-term-occurrence">
-                            <TermLink term={v.term}/>
-                <Badge title={this.props.i18n("resource.metadata.terms.occurrences.suggested.tooltip")}
-                       className="m-term-occurrence-suggested"
-                       color="secondary">{this.props.formatMessage("resource.metadata.terms.occurrences.suggested", {count: v.suggestedCount})}</Badge>
                         </span>);
         });
         return items;
