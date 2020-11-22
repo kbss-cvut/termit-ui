c502a8a49e12d2307679c09944d1e9dcc7dd5539
Petr Křemen <petr.kremen@gmail.com>
2020-11-22 18:56:46 +0100

    [#1430] missing file

---
+++ src/component/term/TermValidationUtils.tsx
@@ -0,0 +1,50 @@
+import Ajax, {params} from "../../util/Ajax";
+import Constants from "../../util/Constants";
+import {IRI} from "../../util/VocabularyUtils";
+import {TermData} from "../../model/Term";
+import {getLocalized} from "../../model/MultilingualString";
+
+export function checkLabelUniqueness(vocabularyIri: IRI,
+                                     prefLabel: string,
+                                     language: string,
+                                     onDuplicate : () => any,
+                                     onUnique? : () => any) {
+    const url = Constants.API_PREFIX + "/vocabularies/" + vocabularyIri.fragment + "/terms";
+    Ajax.head(url, params({
+            namespace: vocabularyIri.namespace,
+            prefLabel,
+            language
+        })
+    ).then(onDuplicate)
+        .catch(onUnique);
+}
+
+function labelInEachLanguageValid<T extends TermData>(data: T, labelExists : LabelExists): boolean {
+    const langs = Object.keys(data.label);
+    for (const lang of langs) {
+        if (!isLabelValid(data, lang) || labelExists[lang]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+export function isTermValid<T extends TermData>(
+    data: T,
+    labelExists: LabelExists,
+) {
+    return data.iri !== undefined
+        && data.iri.trim().length > 0
+        && labelInEachLanguageValid(data, labelExists);
+}
+
+export function isLabelValid<T extends TermData>(
+    data: T,
+    language: string
+) {
+    const localizedLabel = getLocalized(data.label, language);
+    return localizedLabel !== undefined
+        && localizedLabel.trim().length > 0;
+}
+
+export type LabelExists = { [language : string] : boolean }
