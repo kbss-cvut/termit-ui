d54649f769ff96c39cc43601b7fe1b0267688131
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-01-27 17:11:30 +0100

    [Task #1398] Refactor term definition block editing into a separate reusable component.

---
+++ src/component/term/forms/FormUtils.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import ValidationResult from "../../../model/ValidationResult";
+import {getShortLocale} from "../../../util/IntlUtil";
+import {ValidationUtils} from "../validation/ValidationUtils";
+
+export function renderValidationMessages(locale: string, validationResults: ValidationResult[]) {
+    return <>
+        {validationResults.map(r => {
+            const message = r.message.find(ls => ls.language === getShortLocale(locale))!.value;
+            const color = ValidationUtils.qualityAffectingRules.indexOf(r.sourceShape?.iri) > -1 ?
+                ValidationUtils.qualityAffectingRuleViolationColor :
+                ValidationUtils.qualityNotAffectingRuleViolationColor;
+            return <li key={r.iri} style={{color}}>{message}</li>;
+        })}
+    </>;
+}
