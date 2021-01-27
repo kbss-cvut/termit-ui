413a53fb93a7669f9134e0c939ec254351a784f2
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-01-27 09:12:25 +0100

    Render term definition source differently when no definition text is available.

---
+++ src/component/term/TermDefinitionBlock.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import {Col, Row} from "reactstrap";
+import withI18n, {HasI18n} from "../hoc/withI18n";
+import Term from "../../model/Term";
+import {injectIntl} from "react-intl";
+import TermDefinitionContainer from "./TermDefinitionContainer";
+import {getLocalizedOrDefault} from "../../model/MultilingualString";
+import TermDefinitionSource from "./TermDefinitionSource";
+
+export interface TermDefinitionBlockProps extends HasI18n {
+    term: Term;
+    language: string;
+    withDefinitionSource?: boolean;
+}
+
+export const TermDefinitionBlock: React.FC<TermDefinitionBlockProps> = props => {
+    const {term, language} = props;
+    return <TermDefinitionContainer>
+        <Row>
+            <Col xs={12}>
+                <p id="term-metadata-definition"
+                   className="lead mb-1">{getLocalizedOrDefault(term.definition, "", language)}</p>
+            </Col>
+        </Row>
+        <Row>
+            <TermDefinitionSource term={term} language={language} withDefinitionSource={props.withDefinitionSource}/>
+        </Row>
+    </TermDefinitionContainer>;
+};
+
+TermDefinitionBlock.defaultProps = {
+    withDefinitionSource: false
+};
+
+export default injectIntl(withI18n(TermDefinitionBlock));
