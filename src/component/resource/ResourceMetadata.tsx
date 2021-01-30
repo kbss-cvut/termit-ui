a0a84261fa6df34dd690868a006bde22576610c2
Petr Křemen <petr.kremen@gmail.com>
2021-01-30 15:29:49 +0100

    [R1476] fixing html errors

--- src/component/resource/ResourceMetadata.tsx
+++ src/component/resource/ResourceMetadata.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import {injectIntl} from "react-intl";
 import classNames from "classnames";
 import withI18n, {HasI18n} from "../hoc/withI18n";
-import {CardBody, Col, Label, Row} from "reactstrap";
+import {CardBody, Col, Label, Row, Table} from "reactstrap";
 import ResourceTermAssignments from "./ResourceTermAssignments";
 import Resource from "../../model/Resource";
 import ResourceLink from "./ResourceLink";
@@ -16,23 +16,24 @@ interface ResourceMetadataProps extends HasI18n {
 const ResourceMetadata = (props: ResourceMetadataProps) =>
     <div className={classNames({"card": !props.inTab, "mb-3": !props.inTab})}>
         <CardBody className={classNames("card-body-basic-info", {"pl-0": props.inTab})}>
-            {props.inTab && <Row>
-                <Col xl={2} md={4}>
-                    <Label className="attribute-label mb-3">{props.i18n("asset.label")}</Label>
-                </Col>
-                <Col xl={10} md={8}>
-                    <ResourceLink id="resource-metadata-label" resource={props.resource || {}}/>
-                </Col>
-            </Row>}
-            <Row>
-                <Col xl={2} md={4}>
-                    <Label className="attribute-label mb-3">{props.i18n("resource.metadata.description")}</Label>
-                </Col>
-                <Col xl={10} md={8}>
-                    <p id="resource-metadata-description">{(props.resource || {}).description}</p>
-                </Col>
-            </Row>
-            {props.additionalColumns}
+            <Table>
+                {props.inTab && <Row>
+                    <Col xl={2} md={4}>
+                        <Label className="attribute-label mb-3">{props.i18n("asset.label")}</Label>
+                    </Col>
+                    <Col xl={10} md={8}>
+                        <ResourceLink id="resource-metadata-label" resource={props.resource || {}}/>
+                    </Col>
+                </Row>}
+                <Row>
+                    <Col xl={2} md={4}>
+                        <Label className="attribute-label mb-3">{props.i18n("resource.metadata.description")}</Label>
+                    </Col>
+                    <Col xl={10} md={8}>
+                        <p id="resource-metadata-description">{(props.resource || {}).description}</p>
+                    </Col></Row>
+                {props.additionalColumns}
+            </Table>
             <ResourceTermAssignments resource={props.resource}/>
         </CardBody>
     </div>;
