2d65895557eb876ce2e2cb792bae9f9cebf886ad
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-02-25 18:37:34 +0100

    [Fix] Fix action button toolbar rendering in term detail.

--- src/component/misc/HeaderWithActions.tsx
+++ src/component/misc/HeaderWithActions.tsx
@@ -15,7 +15,7 @@ const HeaderWithActions: React.FC<HeaderWithActionsProps> = (props) => (
         <h2 className="page-header">
             {props.title}
         </h2>
-        {props.actions && <ButtonToolbar className="mb-3">{props.actions}</ButtonToolbar>}
+        {props.actions && <ButtonToolbar className="mb-3 align-items-start">{props.actions}</ButtonToolbar>}
     </div>
 )
 
