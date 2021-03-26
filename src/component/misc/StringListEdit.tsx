9dd0fc0034b378aed43096bc8b0910f1836d3bb7
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-03-26 14:35:12 +0100

    [Feature #1288] Replace help text under edit inputs with help icons.

--- src/component/misc/StringListEdit.tsx
+++ src/component/misc/StringListEdit.tsx
@@ -1,144 +1,110 @@
 import * as React from "react";
-import { injectIntl } from "react-intl";
-import withI18n, { HasI18n } from "../hoc/withI18n";
-import {
-  Badge,
-  Button,
-  FormFeedback,
-  FormText,
-  Input,
-  InputGroup,
-  InputGroupAddon,
-  Label,
-} from "reactstrap";
-import { GoPlus } from "react-icons/go";
-import { FaTrashAlt } from "react-icons/fa";
-import Utils from "../../util/Utils";
+import {injectIntl} from "react-intl";
+import withI18n, {HasI18n} from "../hoc/withI18n";
+import {Badge, Button, FormFeedback, Input, InputGroup, InputGroupAddon, Label} from "reactstrap";
+import {GoPlus} from "react-icons/go";
+import {FaTrashAlt} from "react-icons/fa";
 import "./StringListEdit.scss";
+import Utils from "../../util/Utils";
+import HelpIcon from "./HelpIcon";
 
 interface StringListEditProps extends HasI18n {
-  list?: string[];
-  onChange: (list: string[]) => void;
-  i18nPrefix: string;
-  invalid?: boolean;
-  invalidMessage?: JSX.Element;
+    list?: string[];
+    onChange: (list: string[]) => void;
+    i18nPrefix: string;
+    invalid?: boolean;
+    invalidMessage?: JSX.Element;
 }
 
 interface StringListEditState {
-  inputValue: string;
+    inputValue: string;
 }
 
-export class StringListEdit extends React.Component<
-  StringListEditProps,
-  StringListEditState
-> {
-  constructor(props: StringListEditProps) {
-    super(props);
-    this.state = {
-      inputValue: "",
+export class StringListEdit extends React.Component<StringListEditProps, StringListEditState> {
+    constructor(props: StringListEditProps) {
+        super(props);
+        this.state = {
+            inputValue: ""
+        };
+    }
+
+    private onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({inputValue: e.currentTarget.value});
     };
-  }
 
-  private onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ inputValue: e.currentTarget.value });
-  };
+    private onKeyPress = (e: React.KeyboardEvent) => {
+        if (e.key === "Enter") {
+            this.onAdd();
+        }
+    };
 
-  private onKeyPress = (e: React.KeyboardEvent) => {
-    if (e.key === "Enter") {
-      this.onAdd();
+    private onAdd = () => {
+        if (this.state.inputValue.length === 0) {
+            return;
+        }
+        const newList = Utils.sanitizeArray(this.props.list).slice();
+        newList.push(this.state.inputValue);
+        this.props.onChange(newList);
+        this.setState({inputValue: ""});
+    };
+
+    private onRemove = (item: string) => {
+        const newList = Utils.sanitizeArray(this.props.list).slice();
+        newList.splice(newList.indexOf(item), 1);
+        this.props.onChange(newList);
+    };
+
+    private getText = (keySuffix: string) => {
+        return this.props.i18n(this.props.i18nPrefix + "." + keySuffix)
     }
-  };
 
-  private onAdd = () => {
-    if (this.state.inputValue.length === 0) {
-      return;
+    public render() {
+        return <div className="form-group">
+            <Label className="attribute-label">{this.getText("label")}
+                <HelpIcon id={"string-list-edit" + Date.now()} text={this.getText("help")}/>
+            </Label>
+            <InputGroup className="form-group no-bottom-margin">
+                <Input name="add-string-input" value={this.state.inputValue} onChange={this.onChange}
+                       bsSize="sm" onKeyPress={this.onKeyPress}
+                       invalid={this.props.invalid}
+                       placeholder={this.getText("placeholder")}/>
+                <InputGroupAddon addonType="append">
+                    <Button key="add-string-submit" color="primary" size="sm" onClick={this.onAdd}
+                            className="term-edit-source-add-button" disabled={this.state.inputValue.trim().length === 0}
+                            title={this.getText("placeholder.title")}><GoPlus/>&nbsp;{this.getText("placeholder.text")}
+                    </Button>
+                </InputGroupAddon>
+                {this.props.invalid ? <FormFeedback>{this.props.invalidMessage}</FormFeedback> : <></>}
+            </InputGroup>
+            {this.renderList()}
+        </div>;
     }
-    const newList = Utils.sanitizeArray(this.props.list).slice();
-    newList.push(this.state.inputValue);
-    this.props.onChange(newList);
-    this.setState({ inputValue: "" });
-  };
 
-  private onRemove = (item: string) => {
-    const newList = Utils.sanitizeArray(this.props.list).slice();
-    newList.splice(newList.indexOf(item), 1);
-    this.props.onChange(newList);
-  };
-
-  private getText = (keySuffix: string) => {
-    return this.props.i18n(this.props.i18nPrefix + "." + keySuffix);
-  };
-
-  public render() {
-    return (
-      <div className="form-group">
-        <Label className="attribute-label">{this.getText("label")}</Label>
-        <InputGroup className="form-group no-bottom-margin">
-          <Input
-            name="add-string-input"
-            value={this.state.inputValue}
-            onChange={this.onChange}
-            bsSize="sm"
-            onKeyPress={this.onKeyPress}
-            invalid={this.props.invalid}
-            placeholder={this.getText("placeholder")}
-          />
-          <InputGroupAddon addonType="append">
-            <Button
-              key="add-string-submit"
-              color="primary"
-              size="sm"
-              onClick={this.onAdd}
-              className="input-group-button"
-              disabled={this.state.inputValue.trim().length === 0}
-              title={this.getText("placeholder.title")}
-            >
-              <GoPlus />
-              &nbsp;{this.getText("placeholder.text")}
-            </Button>
-          </InputGroupAddon>
-          {this.props.invalid ? (
-            <FormFeedback>{this.props.invalidMessage}</FormFeedback>
-          ) : (
-            <></>
-          )}
-        </InputGroup>
-        <FormText>{this.getText("help")}</FormText>
-        {this.renderList()}
-      </div>
-    );
-  }
-
-  private renderList() {
-    const list = Utils.sanitizeArray(this.props.list);
-    if (list.length === 0) {
-      return null;
+    private renderList() {
+        const list = Utils.sanitizeArray(this.props.list);
+        if (list.length === 0) {
+            return null;
+        }
+        return <table>
+            <tbody>
+            {list.map(s => <tr key={s}>
+                <td className="align-middle">
+                    <ul className="term-items mt-0 mb-0">
+                        <li>
+                            {s}
+                        </li>
+                    </ul>
+                </td>
+                <td className="align-middle">
+                    <Badge title={this.getText("remove.title")}
+                           className="term-edit-source-remove align-middle"
+                           onClick={this.onRemove.bind(null, s)}><FaTrashAlt/> {this.getText("remove.text")}
+                    </Badge>
+                </td>
+            </tr>)}
+            </tbody>
+        </table>;
     }
-    return (
-      <table>
-        <tbody>
-          {list.map((s) => (
-            <tr key={s}>
-              <td className="align-middle">
-                <ul className="term-items mt-0 mb-0">
-                  <li>{s}</li>
-                </ul>
-              </td>
-              <td className="align-middle">
-                <Badge
-                  title={this.getText("remove.title")}
-                  className="list-item-remove-icon align-middle"
-                  onClick={this.onRemove.bind(null, s)}
-                >
-                  <FaTrashAlt /> {this.getText("remove.text")}
-                </Badge>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  }
 }
 
 export default injectIntl(withI18n(StringListEdit));
