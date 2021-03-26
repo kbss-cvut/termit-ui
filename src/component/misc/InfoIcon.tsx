4e151ffb76096f62fa6261d110d9a293a352b123
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-03-26 14:06:42 +0100

    [Feature #1288] Implement a custom help icon with pinned content popover.

--- src/component/misc/InfoIcon.tsx
+++ src/component/misc/InfoIcon.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import {GoInfo} from "react-icons/go";
+import {FaInfoCircle} from "react-icons/fa";
 import {UncontrolledTooltip} from "reactstrap";
 import {Placement} from "popper.js";
 import "./InfoIcon.scss";
 import classNames from "classnames";
 
-interface InfoIconProps {
+export interface InfoIconProps {
     id: string;     // Id of the icon element, necessary for correct tooltip display
     text: string;   // Info message to show
     placement?: Placement; // Where to display the tooltip (relative to the icon). Defaults to "right"
@@ -15,7 +15,7 @@ interface InfoIconProps {
 const InfoIcon: React.FC<InfoIconProps> = (props) => {
     const cls = classNames("info-icon", props.className);
     return <>
-        <GoInfo id={props.id} className={cls}/>
+        <FaInfoCircle id={props.id} className={cls}/>
         <UncontrolledTooltip target={props.id} placement={props.placement}>
             {props.text}
         </UncontrolledTooltip>
