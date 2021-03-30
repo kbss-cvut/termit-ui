5d48aa4358ccc5634a43f4a4f76452c996768948
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-03-30 14:33:46 +0200

    [Feature #1545] Let user select text analysis vocabulary in the annotator.

--- src/component/annotator/TextAnalysisInvocationButton.tsx
+++ src/component/annotator/TextAnalysisInvocationButton.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
+import {injectIntl} from "react-intl";
+import withI18n, {HasI18n} from "../hoc/withI18n";
+import withInjectableLoading, {InjectsLoading} from "../hoc/withInjectableLoading";
 import {GoClippy} from "react-icons/go";
 import {Button} from "reactstrap";
-import {useDispatch} from "react-redux";
+import {connect} from "react-redux";
 import {ThunkDispatch} from "../../util/Types";
 import {executeFileTextAnalysis} from "../../action/AsyncActions";
 import {publishNotification} from "../../action/SyncActions";
@@ -9,39 +12,72 @@ import NotificationType from "../../model/NotificationType";
 import ResourceSelectVocabulary from "../resource/ResourceSelectVocabulary";
 import Vocabulary from "../../model/Vocabulary";
 import {IRI} from "../../util/VocabularyUtils";
-import {useI18n} from "../hook/useI18n";
 
-interface TextAnalysisInvocationButtonProps {
+interface TextAnalysisInvocationButtonProps extends HasI18n, InjectsLoading {
+    id?: string;
     fileIri: IRI;
     defaultVocabularyIri?: string;
+    executeTextAnalysis: (fileIri: IRI, vocabularyIri: string) => Promise<any>;
+    notifyAnalysisFinish: () => void;
     className?: string
 }
 
-const TextAnalysisInvocationButton: React.FC<TextAnalysisInvocationButtonProps> = props => {
-    const {fileIri, defaultVocabularyIri, className} = props;
-    const [showSelector, setShowSelector] = React.useState(false);
-    const dispatch: ThunkDispatch = useDispatch();
-    const {i18n} = useI18n();
-    const close = () => setShowSelector(false);
-    const submit = (voc: Vocabulary | null) => {
-        close();
-        if (!voc) {
-            return;
-        }
-        dispatch(executeFileTextAnalysis(fileIri, voc.iri)).then(() => dispatch(publishNotification({source: {type: NotificationType.TEXT_ANALYSIS_FINISHED}})));
-    };
+interface TextAnalysisInvocationButtonState {
+    showVocabularySelector: boolean;
 
-    return <>
-        <ResourceSelectVocabulary show={showSelector} defaultVocabularyIri={defaultVocabularyIri}
-                                  onCancel={close} onSubmit={submit}/>
-        <Button id="text-analysis-invocation-button"
-                size="sm" color="primary"
-                className={className}
-                title={i18n("file.metadata.startTextAnalysis")}
-                onClick={() => setShowSelector(true)}><GoClippy
-            className="mr-1"/>{i18n("file.metadata.startTextAnalysis.text")}
-        </Button>
-    </>;
 }
 
-export default TextAnalysisInvocationButton;
+export class TextAnalysisInvocationButton extends React.Component<TextAnalysisInvocationButtonProps, TextAnalysisInvocationButtonState> {
+
+    constructor(props: InjectsLoading & TextAnalysisInvocationButtonProps) {
+        super(props);
+        this.state = {showVocabularySelector: false};
+    }
+
+    public onClick = () => {
+        this.setState({showVocabularySelector: true});
+    };
+
+    private invokeTextAnalysis(fileIri: IRI, vocabularyIri: string) {
+        this.props.loadingOn();
+        this.props.executeTextAnalysis(fileIri, vocabularyIri).then(() => {
+            this.props.loadingOff();
+            this.props.notifyAnalysisFinish();
+        });
+    }
+
+    public onVocabularySelect = (vocabulary: Vocabulary | null) => {
+        this.closeVocabularySelect();
+        if (!vocabulary) {
+            return;
+        }
+        this.invokeTextAnalysis(this.props.fileIri, vocabulary.iri);
+    };
+
+    private closeVocabularySelect = () => {
+        this.setState({showVocabularySelector: false});
+    };
+
+    public render() {
+        const i18n = this.props.i18n;
+        return <>
+            <ResourceSelectVocabulary show={this.state.showVocabularySelector}
+                                      defaultVocabularyIri={this.props.defaultVocabularyIri}
+                                      onCancel={this.closeVocabularySelect} onSubmit={this.onVocabularySelect}/>
+            <Button id={this.props.id}
+                    size="sm"
+                    color="primary"
+                    className={this.props.className}
+                    title={i18n("file.metadata.startTextAnalysis")}
+                    onClick={this.onClick}><GoClippy className="mr-1"/>{i18n("file.metadata.startTextAnalysis.text")}
+            </Button>
+        </>;
+    }
+}
+
+export default connect(undefined, (dispatch: ThunkDispatch) => {
+    return {
+        executeTextAnalysis: (fileIri: IRI, vocabularyIri: string) => dispatch(executeFileTextAnalysis(fileIri, vocabularyIri)),
+        notifyAnalysisFinish: () => dispatch(publishNotification({source: {type: NotificationType.TEXT_ANALYSIS_FINISHED}}))
+    };
+})(injectIntl(withI18n(withInjectableLoading(TextAnalysisInvocationButton))));
--- src/component/annotator/__tests__/TextAnalysisInvocationButton.test.tsx
+++ src/component/annotator/__tests__/TextAnalysisInvocationButton.test.tsx
@@ -1,60 +1,125 @@
 import * as React from "react";
-import VocabularyUtils from "../../../util/VocabularyUtils";
+import File from "../../../model/File";
+import VocabularyUtils, {IRI} from "../../../util/VocabularyUtils";
 import Generator from "../../../__tests__/environment/Generator";
 import {shallow} from "enzyme";
-import TextAnalysisInvocationButton from "../TextAnalysisInvocationButton";
-import {intlFunctions, mockUseI18n} from "../../../__tests__/environment/IntlUtil";
+import {TextAnalysisInvocationButton} from "../TextAnalysisInvocationButton";
+import {intlFunctions} from "../../../__tests__/environment/IntlUtil";
+import {InjectsLoading} from "../../hoc/withInjectableLoading";
 import ResourceSelectVocabulary from "../../resource/ResourceSelectVocabulary";
 import Vocabulary from "../../../model/Vocabulary";
-import {ThunkDispatch} from "../../../util/Types";
-import * as redux from "react-redux";
-import * as AsyncActions from "../../../action/AsyncActions";
-import * as SyncActions from "../../../action/SyncActions";
-import {Button} from "reactstrap";
 
 describe("TextAnalysisInvocationButton", () => {
 
+    const namespace = "http://onto.fel.cvut.cz/ontologies/termit/resources/";
+    const fileName = "test.html";
+
+    let file: File;
+
+    let executeTextAnalysis: (fileIri: IRI, vocabularyIri?: string) => Promise<any>;
+    let notifyAnalysisFinish: () => void;
+    let loadingOn: () => void;
+    let loadingOff: () => void;
+    let renderMask: () => JSX.Element | null;
+
+    let loadingProps: InjectsLoading;
     let vocabulary: Vocabulary;
 
-    let dispatch: ThunkDispatch;
-
     beforeEach(() => {
+        file = new File({
+            iri: namespace + fileName,
+            label: fileName,
+            types: [VocabularyUtils.FILE, VocabularyUtils.RESOURCE]
+        });
+        executeTextAnalysis = jest.fn().mockImplementation(() => Promise.resolve());
+        notifyAnalysisFinish = jest.fn();
+        loadingOn = jest.fn();
+        loadingOff = jest.fn();
+        renderMask = jest.fn();
+        loadingProps = {loadingOff, loadingOn, renderMask, loading: false};
         vocabulary = Generator.generateVocabulary();
-        dispatch = jest.fn().mockResolvedValue({});
-        jest.spyOn(redux, "useDispatch").mockReturnValue(dispatch);
-        jest.spyOn(AsyncActions, "executeFileTextAnalysis");
-        jest.spyOn(SyncActions, "publishNotification");
-        mockUseI18n();
+    });
+
+    it("runs text analysis immediately when defaultVocabulary was specified.", () => {
+        const vocabularyIri = Generator.generateUri();
+        vocabulary.iri = vocabularyIri;
+        const fileIri = VocabularyUtils.create(file.iri);
+        const wrapper = shallow<TextAnalysisInvocationButton>(<TextAnalysisInvocationButton fileIri={fileIri}
+                                                                                            executeTextAnalysis={executeTextAnalysis}
+                                                                                            notifyAnalysisFinish={notifyAnalysisFinish}
+                                                                                            defaultVocabularyIri={vocabularyIri}
+                                                                                            {...loadingProps} {...intlFunctions()}/>);
+        wrapper.instance().onVocabularySelect(vocabulary);
+        expect(executeTextAnalysis).toHaveBeenCalledWith(fileIri, vocabularyIri);
+    });
+
+    it("starts loading when text analysis is invoked", () => {
+        const fileIri = VocabularyUtils.create(Generator.generateUri());
+        const vocabularyIri = Generator.generateUri();
+        vocabulary.iri = vocabularyIri;
+        const wrapper = shallow<TextAnalysisInvocationButton>(<TextAnalysisInvocationButton fileIri={fileIri}
+                                                                                            executeTextAnalysis={executeTextAnalysis}
+                                                                                            notifyAnalysisFinish={notifyAnalysisFinish}
+                                                                                            defaultVocabularyIri={vocabularyIri}
+                                                                                            {...loadingProps} {...intlFunctions()}/>);
+        wrapper.instance().onVocabularySelect(vocabulary);
+        expect(loadingOn).toHaveBeenCalled();
+    });
+
+    it("stops loading after text analysis invocation finishes", () => {
+        const fileIri = VocabularyUtils.create(Generator.generateUri());
+        const vocabularyIri = Generator.generateUri();
+        vocabulary.iri = vocabularyIri;
+        const wrapper = shallow<TextAnalysisInvocationButton>(<TextAnalysisInvocationButton fileIri={fileIri}
+                                                                                            executeTextAnalysis={executeTextAnalysis}
+                                                                                            notifyAnalysisFinish={notifyAnalysisFinish}
+                                                                                            defaultVocabularyIri={vocabularyIri}
+                                                                                            {...loadingProps} {...intlFunctions()}/>);
+        wrapper.instance().onVocabularySelect(vocabulary);
+        return Promise.resolve().then(() => {
+            expect(loadingOff).toHaveBeenCalled();
+        });
     });
 
     it("publishes notification after text analysis invocation finishes", () => {
         const fileIri = VocabularyUtils.create(Generator.generateUri());
         const vocabularyIri = Generator.generateUri();
         vocabulary.iri = vocabularyIri;
-        const wrapper = shallow(<TextAnalysisInvocationButton fileIri={fileIri}
-                                                              defaultVocabularyIri={vocabularyIri} {...intlFunctions()}/>);
-        wrapper.find(ResourceSelectVocabulary).prop("onSubmit")(vocabulary);
+        const wrapper = shallow<TextAnalysisInvocationButton>(<TextAnalysisInvocationButton fileIri={fileIri}
+                                                                                            executeTextAnalysis={executeTextAnalysis}
+                                                                                            notifyAnalysisFinish={notifyAnalysisFinish}
+                                                                                            defaultVocabularyIri={vocabularyIri}
+                                                                                            {...loadingProps} {...intlFunctions()}/>);
+        wrapper.instance().onVocabularySelect(vocabulary);
         return Promise.resolve().then(() => {
-            expect(SyncActions.publishNotification).toHaveBeenCalled();
+            expect(notifyAnalysisFinish).toHaveBeenCalled();
         });
     });
 
-    it("shows vocabulary selector on button click", () => {
+    it("shows vocabulary selector when no default vocabulary was specified", () => {
         const fileIri = VocabularyUtils.create(Generator.generateUri());
-        const wrapper = shallow(<TextAnalysisInvocationButton fileIri={fileIri} {...intlFunctions()}/>);
-        wrapper.find(Button).simulate("click");
+        const wrapper = shallow<TextAnalysisInvocationButton>(<TextAnalysisInvocationButton fileIri={fileIri}
+                                                                                            executeTextAnalysis={executeTextAnalysis}
+                                                                                            notifyAnalysisFinish={notifyAnalysisFinish}
+                                                                                            {...loadingProps} {...intlFunctions()}/>);
+        wrapper.instance().onClick();
         wrapper.update();
+        expect(wrapper.instance().state.showVocabularySelector).toBeTruthy();
         expect(wrapper.exists(ResourceSelectVocabulary));
-        expect(wrapper.find(ResourceSelectVocabulary).prop("show")).toBeTruthy();
+        expect(executeTextAnalysis).not.toHaveBeenCalled();
     });
 
     it("invokes text analysis with selected Vocabulary when Vocabulary selector is submitted", () => {
-        const fileIri = VocabularyUtils.create(Generator.generateUri());
-        const vocabularyIri = Generator.generateUri();
-        vocabulary.iri = vocabularyIri;
-        const wrapper = shallow(<TextAnalysisInvocationButton fileIri={fileIri}
-                                                              defaultVocabularyIri={vocabularyIri} {...intlFunctions()}/>);
-        wrapper.find(ResourceSelectVocabulary).prop("onSubmit")(vocabulary);
-        expect(AsyncActions.executeFileTextAnalysis).toHaveBeenCalledWith(fileIri, vocabularyIri)
+        const wrapper = shallow<TextAnalysisInvocationButton>(<TextAnalysisInvocationButton fileIri={VocabularyUtils.create(file.iri)}
+                                                                                            executeTextAnalysis={executeTextAnalysis}
+                                                                                            notifyAnalysisFinish={notifyAnalysisFinish}
+                                                                                            {...loadingProps} {...intlFunctions()}/>);
+        wrapper.instance().onClick();
+        wrapper.update();
+        expect(wrapper.instance().state.showVocabularySelector).toBeTruthy();
+        wrapper.instance().onVocabularySelect(vocabulary);
+        expect(executeTextAnalysis).toHaveBeenLastCalledWith(VocabularyUtils.create(file.iri), vocabulary.iri);
+        wrapper.update();
+        expect(wrapper.instance().state.showVocabularySelector).toBeFalsy();
     });
 });
