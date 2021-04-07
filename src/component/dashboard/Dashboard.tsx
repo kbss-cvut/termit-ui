221505a98fb9d782935225955224e384d53e7b2c
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-04-07 08:01:38 +0200

    [Task #1547] Use raw.macro to allow using SPARQL queries directly from frontend (IMHO bad idea).

--- src/component/dashboard/Dashboard.tsx
+++ src/component/dashboard/Dashboard.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import {Col, Row} from "reactstrap";
 import TermFrequency from "../statistics/termfrequency/TermFrequency";
 import PanelWithActions from "../misc/PanelWithActions";
-import templateQuery from "../statistics/termfrequency/TermFrequency.rq";
 import LastEditedAssets from "./widget/lastedited/LastEditedAssets";
 import "./Dashboard.scss";
 import NewsAlert from "./widget/NewsAlert";
@@ -10,6 +9,7 @@ import Constants from "../../util/Constants";
 import WindowTitle from "../misc/WindowTitle";
 import {useI18n} from "../hook/useI18n";
 import LastCommentedAssets from "./widget/lastcommented/LastCommentedAssets";
+import raw from "raw.macro";
 
 const Dashboard: React.FC = () => {
     const {i18n, locale} = useI18n();
@@ -25,7 +25,7 @@ const Dashboard: React.FC = () => {
             </Col>
             <Col xl={4} lg={6} md={12}>
                 <PanelWithActions className="p-0" title={i18n("dashboard.widget.typeFrequency.title")}>
-                    <TermFrequency sparqlQuery={templateQuery} lang={locale}/>
+                    <TermFrequency sparqlQuery={raw("../statistics/termfrequency/TermFrequency.rq")} lang={locale}/>
                 </PanelWithActions>
             </Col>
         </Row>
