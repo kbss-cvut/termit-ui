8b3477d0b19d3407224d57e58fdc03a5c8c01533
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-04-14 08:48:20 +0200

    Format code using Prettier.

--- src/component/annotator/LegendToggle.tsx
+++ src/component/annotator/LegendToggle.tsx
@@ -1,30 +1,30 @@
 import * as React from "react";
 import Legend from "./Legend";
-import {Button, Card, CardBody, Collapse} from "reactstrap";
-import {useI18n} from "../hook/useI18n";
+import { Button, Card, CardBody, Collapse } from "reactstrap";
+import { useI18n } from "../hook/useI18n";
 
 const LegendToggle = () => {
   const [showLegend, setShowLegend] = React.useState(true);
-  const {i18n} = useI18n();
+  const { i18n } = useI18n();
   const toggle = () => setShowLegend(!showLegend);
   return (
-      <>
-        <Collapse isOpen={showLegend}>
-          <Card className="legend mb-0">
-            <CardBody>
-              <Legend />
-            </CardBody>
-          </Card>
-        </Collapse>
-        <Button
-            color="primary"
-            size="sm"
-            onClick={toggle}
-            className="legend-button"
-        >
-          {i18n(`annotator.legend.toggle.${!showLegend ? "show" : "hide"}`)}
-        </Button>
-      </>
+    <>
+      <Collapse isOpen={showLegend}>
+        <Card className="legend mb-0">
+          <CardBody>
+            <Legend />
+          </CardBody>
+        </Card>
+      </Collapse>
+      <Button
+        color="primary"
+        size="sm"
+        onClick={toggle}
+        className="legend-button"
+      >
+        {i18n(`annotator.legend.toggle.${!showLegend ? "show" : "hide"}`)}
+      </Button>
+    </>
   );
 };
 
