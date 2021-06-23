aca5afc262fb252b5e5a378e37d62ad35ce15628
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-06-23 10:45:52 +0200

    [Format] Apply Prettier formatting.

--- src/util/Constants.ts
+++ src/util/Constants.ts
@@ -114,7 +114,7 @@ const deployment =
   constants.DEPLOYMENT_NAME.length > 0 ? constants.DEPLOYMENT_NAME + "-" : "";
 constants.STORAGE_JWT_KEY = `${constants.APP_NAME}-${deployment}${constants.Headers.AUTHORIZATION}`;
 constants.STORAGE_LANG_KEY = `${constants.APP_NAME}-${deployment}LANG`;
-constants.STORAGE_TABLE_PAGE_SIZE_KEY =`${constants.APP_NAME}-${deployment}TABLE_PAGE_SIZE`;
+constants.STORAGE_TABLE_PAGE_SIZE_KEY = `${constants.APP_NAME}-${deployment}TABLE_PAGE_SIZE`;
 constants.STORAGE_ANNOTATOR_LEGEND_OPEN_KEY = `${constants.APP_NAME}-${deployment}ANNOTATOR_LEGEND_OPEN`;
 
 export default constants;
