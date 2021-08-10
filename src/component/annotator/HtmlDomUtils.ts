1003d510f3c8922a5a2aa8a308ddef210039ffbb
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-08-10 08:37:04 +0200

    [Bud #1610] Rewrite extendSelectionToWords to rely less on the buggy implementation of selection.modify in Firefox.

--- src/component/annotator/HtmlDomUtils.ts
+++ src/component/annotator/HtmlDomUtils.ts
@@ -40,7 +40,7 @@ const BLOCK_ELEMENTS = [
   "ul",
 ];
 
-function calculatePathLength(node: Node, ancestor: Node) {
+function calculatePathLength(node: Node, ancestor: Node): number {
   let parent = node.parentNode;
   let length = 0;
   while (parent && parent !== ancestor) {
@@ -48,6 +48,19 @@ function calculatePathLength(node: Node, ancestor: Node) {
     parent = parent.parentNode;
   }
   return length;
+}
+
+/**
+ * Detects if the specified selection is backwards.
+ * @param sel Selection to check
+ */
+function isBackwards(sel: Selection): boolean {
+  const range = document.createRange();
+  range.setStart(sel.anchorNode!, sel.anchorOffset);
+  range.setEnd(sel.focusNode!, sel.focusOffset);
+  const backwards = range.collapsed;
+  range.detach();
+  return backwards;
 }
 
 const HtmlDomUtils = {
@@ -86,41 +99,48 @@ const HtmlDomUtils = {
 
   extendSelectionToWords() {
     const sel = window.getSelection();
-    // @ts-ignore
-    if (sel && !sel.isCollapsed && sel.modify) {
-      // Detect if selection is backwards
-      const range = document.createRange();
+    if (
+      sel &&
+      !sel.isCollapsed &&
+      sel.anchorNode &&
+      sel.focusNode &&
       // @ts-ignore
-      range.setStart(sel.anchorNode, sel.anchorOffset);
-      // @ts-ignore
-      range.setEnd(sel.focusNode, sel.focusOffset);
-      const backwards = range.collapsed;
-      range.detach();
-
+      sel.modify
+    ) {
+      const backwards = isBackwards(sel);
       // modify() works on the focus of the selection
       const endNode = sel.focusNode;
-      const endOffset =
-        sel.focusOffset !== 0 ? sel.focusOffset - 1 : sel.focusOffset;
+      const endOffset = Math.max(0, sel.focusOffset - 1);
       sel.collapse(
         sel.anchorNode,
         backwards ? sel.anchorOffset : sel.anchorOffset + 1
       );
       if (backwards) {
-        // @ts-ignore
-        sel.modify("move", "forward", "word");
-        // @ts-ignore
-        sel.extend(endNode, endOffset);
-        // @ts-ignore
-        sel.modify("extend", "backward", "word");
+        // Note that we are not using sel.modify by word due to issues with Firefox messing up the modification/extension
+        // in certain situations (Bug #1610)
+        const anchorText = sel.anchorNode.textContent || "";
+        while (
+          anchorText.charAt(sel.anchorOffset).trim().length !== 0 &&
+          sel.anchorOffset < anchorText.length
+        ) {
+          // @ts-ignore
+          sel.modify("move", "forward", "character");
+        }
+        const text = endNode.textContent || "";
+        let index = endOffset;
+        while (text.charAt(index).trim().length !== 0 && index >= 0) {
+          index--;
+        }
+        sel.extend(endNode, index + 1);
       } else {
         // @ts-ignore
         sel.modify("move", "backward", "word");
-        const text = endNode!.textContent || "";
+        const text = endNode.textContent || "";
         let index = endOffset;
         while (text.charAt(index).trim().length !== 0 && index < text.length) {
           index++;
         }
-        sel.extend(endNode!, index);
+        sel.extend(endNode, index);
       }
     }
   },
