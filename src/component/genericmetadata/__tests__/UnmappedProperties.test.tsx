5952f4e97f843574c282e665d3a2633be5bcdf7c
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-12-19 11:59:30 +0100

    Display unmapped property value sorted.

--- src/component/genericmetadata/__tests__/UnmappedProperties.test.tsx
+++ src/component/genericmetadata/__tests__/UnmappedProperties.test.tsx
@@ -1,10 +1,15 @@
 import Generator from "../../../__tests__/environment/Generator";
 import UnmappedProperties from "../UnmappedProperties";
 import { mountWithIntl } from "../../../__tests__/environment/Environment";
+import { mockUseI18n } from "../../../__tests__/environment/IntlUtil";
 
 jest.mock("../../misc/AssetLabel", () => () => <span>Asset</span>);
 
 describe("UnmappedProperties", () => {
+  beforeEach(() => {
+    mockUseI18n();
+  });
+
   it("renders literal value for a property", () => {
     const properties = new Map([[Generator.generateUri(), ["test"]]]);
     const wrapper = mountWithIntl(
@@ -29,12 +34,26 @@ describe("UnmappedProperties", () => {
   it("handles object value for a property", () => {
     const v = Generator.generateUri();
     const properties = new Map([[Generator.generateUri(), [{ iri: v }]]]);
-    // @ts-ignore
     const wrapper = mountWithIntl(
       <UnmappedProperties properties={properties} />
     );
     const items = wrapper.find("li");
     expect(items.length).toEqual(1);
     expect(items.get(0).props.children).toEqual(v);
+  });
+
+  it("renders multiple property values sorted lexicographically", () => {
+    const values = ["z", "a", "j", "i", "c"];
+    const sortedValues = [...values];
+    sortedValues.sort((a, b) => a.localeCompare(b));
+    const properties = new Map([[Generator.generateUri(), values]]);
+    const wrapper = mountWithIntl(
+      <UnmappedProperties properties={properties} />
+    );
+    const items = wrapper.find("li");
+    expect(items.length).toEqual(values.length);
+    for (let i = 0; i < sortedValues.length; i++) {
+      expect(items.get(i).props.children).toEqual(sortedValues[i]);
+    }
   });
 });
