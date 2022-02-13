49f895c844b65227ff7edccd259509169e55c47d
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2022-02-13 18:56:56 +0100

    [Upd] Migrate from deprecated node-sass to reference sass implementation.
    Migrate from deprecated redux-devtools-extension to @redux-devtools/extension.

--- src/store/TermItStore.ts
+++ src/store/TermItStore.ts
@@ -2,7 +2,7 @@ import { applyMiddleware, createStore, Middleware } from "redux";
 import { createLogger } from "redux-logger";
 import thunk, { ThunkMiddleware } from "redux-thunk";
 import TermItReducers from "../reducer/TermItReducers";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "@redux-devtools/extension";
 import TermItState from "../model/TermItState";
 
 const middlewares: Middleware[] = [thunk as ThunkMiddleware];
