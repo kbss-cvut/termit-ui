c9ddc1083f6ad5dc9aa174cc4aecd3bcad9d9d49
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2022-03-16 08:44:13 +0100

    Simplify DynamicBreadcrumbRoute.tsx, use hooks.

--- src/component/breadcrumb/DynamicBreadcrumbRoute.tsx
+++ src/component/breadcrumb/DynamicBreadcrumbRoute.tsx
@@ -2,34 +2,26 @@ import * as React from "react";
 import { Route, RouteComponentProps, RouteProps } from "react-router-dom";
 import { Breadcrumb } from "react-breadcrumbs";
 import TermItState from "../../model/TermItState";
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 import Asset from "../../model/Asset";
 import { useI18n } from "../hook/useI18n";
 
-interface DynamicBreadcrumbRouteOwnProps extends RouteProps {
+interface DynamicBreadcrumbRouteProps extends RouteProps {
   component: React.ComponentType<any>;
   includeSearch?: boolean;
   asset: string;
 }
-
-interface DynamicBreadcrumbRouteStoreProps {
-  state: TermItState;
-}
-
-declare type DynamicBreadcrumbRouteProps = DynamicBreadcrumbRouteOwnProps &
-  DynamicBreadcrumbRouteStoreProps;
 
 // This route should extract breadcrumb label from store data, e.g., vocabulary label from currently open vocabulary
 const DynamicBreadcrumbRoute = (props: DynamicBreadcrumbRouteProps) => {
   const { component, includeSearch, ...rest } = { ...props };
   const Component = component; // lowercase first character does not pass through JSX validation
   const { locale } = useI18n();
+  const asset = useSelector((state: TermItState) => state[props.asset]);
   const renderRoute = (routeProps: RouteComponentProps<any>) => (
     <Breadcrumb
       data={{
-        title: props.state[props.asset]
-          ? (props.state[props.asset] as Asset).getLabel(locale)
-          : "",
+        title: asset ? (asset as Asset).getLabel(locale) : "",
         pathname: routeProps.match.url,
         search: includeSearch ? routeProps.location.search : undefined,
       }}
@@ -41,11 +33,4 @@ const DynamicBreadcrumbRoute = (props: DynamicBreadcrumbRouteProps) => {
   return <Route {...rest} render={renderRoute} />;
 };
 
-export default connect<
-  DynamicBreadcrumbRouteStoreProps,
-  undefined,
-  DynamicBreadcrumbRouteOwnProps,
-  TermItState
->((state: TermItState) => {
-  return { state };
-})(DynamicBreadcrumbRoute);
+export default DynamicBreadcrumbRoute;
