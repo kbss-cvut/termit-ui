0dd9e069767a18e5edbd186b62c0418055c5bc14
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2022-05-04 10:53:59 +0200

    [Upd] Update dependencies.

--- src/component/footer/__tests__/FooterMenu.test.tsx
+++ src/component/footer/__tests__/FooterMenu.test.tsx
@@ -1,11 +1,13 @@
 import { FooterMenu } from "../FooterMenu";
-import { mountWithIntl } from "../../../__tests__/environment/Environment";
+import {
+  mockStore,
+  mountWithIntl,
+} from "../../../__tests__/environment/Environment";
 import { LanguageSelector } from "../../main/LanguageSelector";
-import * as redux from "react-redux";
 
 describe("FooterMenu", () => {
   it("renders component correctly", () => {
-    jest.spyOn(redux, "useSelector").mockReturnValue(true);
+    mockStore.getState().sidebarExpanded = true;
     const wrapper = mountWithIntl(<FooterMenu fixed={true} />);
 
     expect(wrapper.find("div").find(LanguageSelector).length).toBe(1);
