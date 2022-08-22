1795c4c0464f9dcfa367938abca2d46bd62c8eb5
Filip Kopecký <kopeckyfil@gmail.com>
2022-08-22 14:47:21 +0200

    [#165] code refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,18 @@ import * as React from "react";
 import { Provider } from "react-redux";
 import TermItStore from "./store/TermItStore";
 import { ReactKeycloakProvider } from "@react-keycloak/web";
-import { AuthClientTokens } from "@react-keycloak/core";
 import IntlApp from "./IntlApp";
 import keycloak from "./util/Keycloak";
 import SecurityUtils from "./util/SecurityUtils";
 import IntlAppKeycloak from "./IntlAppKeycloak";
 
-const tokenSaver = (tokens: AuthClientTokens) => {
-  if (tokens.token) {
-    SecurityUtils.saveToken("Bearer " + tokens.token);
-  }
-};
-
 const App: React.FC = () => {
   if (process.env.REACT_APP_AUTHENTICATION === "keycloak") {
     return (
-      <ReactKeycloakProvider authClient={keycloak} onTokens={tokenSaver}>
+      <ReactKeycloakProvider
+        authClient={keycloak}
+        onTokens={SecurityUtils.tokenSaver}
+      >
         <Provider store={TermItStore}>
           <IntlAppKeycloak />
         </Provider>
