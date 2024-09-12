74f205e10cb0f9d2e066d41567a4dd7c418c0f90
lukaskabc <lukaskabc@gmail.com>
2024-09-12 12:03:40 +0200

    [Performance #285] change long running task registry handling

--- src/reducer/WebSocketUtilityDispatchers.ts
+++ src/reducer/WebSocketUtilityDispatchers.ts
@@ -2,30 +2,22 @@ import { IMessage } from "react-stomp-hooks";
 import { Action } from "redux";
 import { ThunkDispatch } from "../util/Types";
 import { LongRunningTask } from "../model/LongRunningTask";
-import {
-  asyncActionSuccess,
-  asyncActionSuccessWithPayload,
-} from "../action/SyncActions";
+import { asyncActionSuccessWithPayload } from "../action/SyncActions";
 
 export function updateLongRunningTasks(message: IMessage, action: Action) {
   return async (dispatch: ThunkDispatch) => {
-    if (message.headers["reset"]) {
-      dispatch(asyncActionSuccess(action));
-      return;
-    }
+    const tasks: [LongRunningTask] = JSON.parse(message.body);
+    const mapped: { [uuid: string]: LongRunningTask } = {};
 
-    const tasks: { [uuid: string]: LongRunningTask } = JSON.parse(message.body);
-
-    const mapped = Object.keys(tasks).map((uuid) => {
-      const task = tasks[uuid];
+    tasks.forEach((task) => {
       if (task.startedAt) {
         // @ts-ignore
         task.startedAt = new Date(task.startedAt * 1000);
-        task.startedAt.setMilliseconds(0); // round to second
       }
 
       task.name = "longrunningtasks.name." + task.name;
-      return task;
+
+      mapped[task.uuid] = task;
     });
 
     dispatch(asyncActionSuccessWithPayload(action, mapped));
