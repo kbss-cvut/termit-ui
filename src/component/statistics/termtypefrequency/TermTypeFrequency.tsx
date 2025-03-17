70dc64fed96207ff160897b56d6d697b0d22f8f6
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2025-03-17 10:08:34 +0100

    [GH-611] Use server-side endpoint for term type distribution statistics

--- src/component/statistics/termtypefrequency/TermTypeFrequency.tsx
+++ src/component/statistics/termtypefrequency/TermTypeFrequency.tsx
@@ -1,127 +1,89 @@
 import * as React from "react";
-import SparqlWidget, { PublicProps } from "../SparqlWidget";
 import Chart from "react-apexcharts";
-import LD from "ld-query";
-import VocabularyUtils from "../../../util/VocabularyUtils";
 import defaultChartOptions from "../DefaultTermCharacteristicsFrequencyChartOptions";
-import withInjectableLoading from "../../hoc/withInjectableLoading";
+import { useI18n } from "../../hook/useI18n";
+import { TermTypeDistributionDto } from "../../../model/statistics/TermTypeDistributionDto";
+import { useDispatch } from "react-redux";
+import { ThunkDispatch } from "../../../util/Types";
+import { loadTermTypeDistributionStatistics } from "../../../action/AsyncStatisticsActions";
+import { trackPromise } from "react-promise-tracker";
+import { getLocalized } from "../../../model/MultilingualString";
+import { getShortLocale } from "../../../util/IntlUtil";
+import { RdfsResourceData } from "../../../model/RdfsResource";
+import PromiseTrackingMask from "../../misc/PromiseTrackingMask";
 
-const TYPE_NOT_FILLED = VocabularyUtils.PREFIX + "not-filled";
+const TermTypeFrequency: React.FC = () => {
+  const { i18n, locale } = useI18n();
+  const lang = getShortLocale(locale);
+  const [data, setData] = React.useState<TermTypeDistributionDto[] | null>(
+    null
+  );
+  const dispatch: ThunkDispatch = useDispatch();
 
-const TYPES = [
-  "https://slovník.gov.cz/základní/pojem/typ-objektu",
-  "https://slovník.gov.cz/základní/pojem/typ-vlastnosti",
-  "https://slovník.gov.cz/základní/pojem/typ-vztahu",
-  "https://slovník.gov.cz/základní/pojem/typ-události",
-  TYPE_NOT_FILLED,
-];
+  React.useEffect(() => {
+    trackPromise(
+      dispatch(loadTermTypeDistributionStatistics()),
+      "term-type-distribution"
+    ).then((data) => setData(data));
+  }, [dispatch]);
 
-interface Props extends PublicProps {
-  empty: string;
-  notFilled: string;
-  lang: string;
-}
-
-class TermTypeFrequency extends React.Component<Props> {
-  private cx = LD({
-    p: VocabularyUtils.PREFIX,
-    rdfs: VocabularyUtils.PREFIX_RDFS,
+  const options = Object.assign(defaultChartOptions, {
+    xaxis: {
+      categories: (data || []).map((item) =>
+        getLocalized(item.resource.label, lang)
+      ),
+    },
   });
 
-  private getLabel(res: any, iri: string) {
-    if (TYPE_NOT_FILLED === iri) {
-      return this.props.notFilled;
-    }
-    const labels = this.cx(res).queryAll("[@id=" + iri + "] rdfs:label");
-    for (const label of labels) {
-      const q = label.query("@language");
-      if (q && q.json() === this.props.lang) {
-        return label.query("@value");
-      }
-    }
-    return this.cx(res).query("[@id=" + iri + "] rdfs:label @value");
+  const types = new Map<string, RdfsResourceData>();
+  const typeDistribution = new Map<string, Map<string, number>>();
+  (data || []).forEach((dto) => {
+    const vocMap = new Map<string, number>();
+    typeDistribution.set(dto.resource.iri, vocMap);
+    dto.typeDistribution.forEach((dt) => {
+      types.set(dt.resource.iri, dt.resource);
+      vocMap.set(dt.resource.iri, dt.count);
+    });
+  });
+
+  const series: { name: string; data: number[] }[] = [];
+  types.forEach((item) => {
+    const dataArr: number[] = [];
+    typeDistribution.forEach((value) => {
+      dataArr.push(value.get(item.iri) || 0);
+    });
+    series.push({
+      name: getLocalized(item.label, lang),
+      data: dataArr,
+    });
+  });
+
+  let component: React.ReactNode;
+  if (!data) {
+    return null;
   }
-
-  public render() {
-    const TermTypeFrequencyI = this;
-    const queryResult = this.props.queryResults;
-    if (!queryResult || !queryResult.result) {
-      return <div>{this.props.renderMask()}</div>;
-    }
-
-    const res = queryResult.result;
-    const types: object = {};
-
-    TYPES.forEach((t) => (types[t] = TermTypeFrequencyI.getLabel(res, t) || t));
-
-    const vocabularies = {};
-    res
-      .filter((r: any) => {
-        return r[VocabularyUtils.IS_TERM_FROM_VOCABULARY] !== undefined;
-      })
-      .forEach((r: any) => {
-        const voc = r[VocabularyUtils.IS_TERM_FROM_VOCABULARY][0];
-        const vocabulary = voc["@id"];
-        let vocO = vocabularies[vocabulary];
-        if (!vocO) {
-          vocO = { name: TermTypeFrequencyI.getLabel(res, vocabulary) };
-          Object.keys(types).forEach((type) => {
-            vocO[types[type]] = 0;
-          });
-          vocabularies[vocabulary] = vocO;
-        }
-
-        let currType;
-        if (r["@type"]) {
-          currType = r["@type"];
-        } else if (r[VocabularyUtils.RDF_TYPE]) {
-          // GraphDB returns types as rdf:type property values
-          currType = r[VocabularyUtils.RDF_TYPE].map((t: any) => t["@id"]);
-        } else {
-          currType = TYPE_NOT_FILLED;
-        }
-
-        let found = false;
-        currType.forEach((tt: any) => {
-          if (types[tt]) {
-            vocO[types[tt]] = vocO[types[tt]] + 1 || 1;
-            found = true;
-          }
-        });
-        if (!found) {
-          vocO[types[TYPE_NOT_FILLED]] += 1;
-        }
-      });
-
-    const series = Object.keys(types).map((t) => {
-      return {
-        name: types[t],
-        data: Object.keys(vocabularies).map((v) => vocabularies[v][types[t]]),
-      };
-    });
-
-    const options = Object.assign(defaultChartOptions, {
-      xaxis: {
-        categories: Object.keys(vocabularies).map((v) => vocabularies[v].name),
-      },
-    });
-
+  if (data.length === 0) {
+    component = <h4>{i18n("statistics.types.frequency.empty")}</h4>;
+  } else {
     // Height is given by the number of vocabularies + a fixed value for the legend
-    if (Object.keys(vocabularies).length === 0) {
-      return <h4>{this.props.empty}</h4>;
-    } else {
-      const height = Object.keys(vocabularies).length * 35 + 60;
-      return (
-        <Chart
-          options={options}
-          series={series}
-          type="bar"
-          width="100%"
-          height={`${height}px`}
-        />
-      );
-    }
+    const height = data.length * 35 + 60;
+    component = (
+      <Chart
+        options={options}
+        series={series}
+        type="bar"
+        width="100%"
+        height={`${height}px`}
+      />
+    );
   }
-}
 
-export default withInjectableLoading(SparqlWidget(TermTypeFrequency));
+  return (
+    <>
+      <PromiseTrackingMask area="term-type-distribution" />
+      {component}
+    </>
+  );
+};
+
+export default TermTypeFrequency;
