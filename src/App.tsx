5417345fed73c0c03213eac1723238fb1a450453
Hubert Aschermann <hubert@aschermann.cz>
2026-04-14 19:15:36 +0200

    [Feat #751] Sheet-like vocabulary display

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,35 @@ import IntlApp from "./IntlApp";
 import { isUsingOidcAuth } from "./util/OidcUtils";
 import OidcIntlApp from "./OidcIntlApp";
 import { WebSocketWrapper } from "./WebSocketApp";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
 
 const App: React.FC = () => {
-  if (isUsingOidcAuth()) {
-    return (
-      <Provider store={TermItStore}>
-        <WebSocketWrapper>
-          <OidcIntlApp />
-        </WebSocketWrapper>
-      </Provider>
-    );
-  } else {
-    return (
-      <Provider store={TermItStore}>
-        <WebSocketWrapper>
-          <IntlApp />
-        </WebSocketWrapper>
-      </Provider>
-    );
-  }
+  return (
+    <QueryClientProvider client={queryClient}>
+      {isUsingOidcAuth() ? (
+        <Provider store={TermItStore}>
+          <WebSocketWrapper>
+            <OidcIntlApp />
+          </WebSocketWrapper>
+        </Provider>
+      ) : (
+        <Provider store={TermItStore}>
+          <WebSocketWrapper>
+            <IntlApp />
+          </WebSocketWrapper>
+        </Provider>
+      )}
+    </QueryClientProvider>
+  );
 };
 
 export default App;
