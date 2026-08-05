2880391446e8d42b0fd73ea440e1de581ed8c756
Hubert Aschermann <hubert@aschermann.cz>
2026-08-05 16:55:00 +0200

    [Ref #753] Combine related/imported lists into one new component

--- src/component/vocabulary/VocabulariesReferenceList.tsx
+++ src/component/vocabulary/VocabulariesReferenceList.tsx
@@ -3,28 +3,29 @@ import { AssetData } from "../../model/Asset";
 import Utils from "../../util/Utils";
 import { Col, Label, Row } from "reactstrap";
 import VocabularyIriLink from "./VocabularyIriLink";
-import "./ImportedVocabulariesList.scss";
+import "./VocabulariesReferenceList.scss";
 import { useI18n } from "../hook/useI18n";
 
-interface RelatedVocabulariesListProps {
+interface VocabulariesReferenceListProps {
   vocabularies?: AssetData[];
+  labelKey: string;
+  htmlId: string;
 }
 
-export const RelatedVocabulariesList: React.FC<RelatedVocabulariesListProps> = (
-  props: RelatedVocabulariesListProps
-) => {
+export const VocabulariesReferenceList: React.FC<
+  VocabulariesReferenceListProps
+> = ({ vocabularies, labelKey, htmlId }) => {
   const { i18n } = useI18n();
-  const vocabs = Utils.sanitizeArray(props.vocabularies);
+  const vocabs = Utils.sanitizeArray(vocabularies);
   vocabs.sort((a: AssetData, b: AssetData) => a.iri!.localeCompare(b.iri!));
+
   return (
     <Row>
       <Col xl={2} md={4}>
-        <Label className="attribute-label mb-3">
-          {i18n("vocabulary.detail.related")}
-        </Label>
+        <Label className="attribute-label mb-3">{i18n(labelKey as any)}</Label>
       </Col>
       <Col xl={10} md={8}>
-        <ul id="vocabulary-metadata-relatedVocabularies" className="ul-padding">
+        <ul id={htmlId} className="ul-padding">
           {vocabs.map((v) => (
             <li key={v.iri}>
               <VocabularyIriLink iri={v.iri!} />
@@ -36,4 +37,4 @@ export const RelatedVocabulariesList: React.FC<RelatedVocabulariesListProps> = (
   );
 };
 
-export default RelatedVocabulariesList;
+export default VocabulariesReferenceList;
