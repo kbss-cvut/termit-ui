543a1a0ab37b52bfc0ba61c628a8a08526368c4e
Hubert Aschermann <hubert@aschermann.cz>
2026-09-02 22:46:05 +0200

    [Feat #754] Add a list of selected terms to the side bar

--- src/component/vocabulary/sheet/sidebar/TermEditSidebar.tsx
+++ src/component/vocabulary/sheet/sidebar/TermEditSidebar.tsx
@@ -17,12 +17,33 @@ export interface TermEditSidebarProps {
 
   inBatchMode?: boolean;
   vocabularyIri: string;
-  targetTerms?: Set<string>;
+  selectedTerms?: Term[];
+  selectedTermIris?: Set<string>;
   onBatchSave?: (
     updatedTerm: Omit<TermBatchEditDto, "targetTerms">
   ) => Promise<void>;
 }
 
+/**
+ * A sidebar component for editing a term or batch of terms in a vocabulary
+ * sheet.
+ * It can operate in two modes: single term edit mode and batch edit mode. In
+ * single term edit mode, it displays a TermCellEditor for editing a specific
+ * term. In batch edit mode, it displays a BatchTermEditor for editing
+ * properties of multiple terms.
+ * @param isOpen whether the sidebar is open or closed.
+ * @param language the current language for displaying term information.
+ * @param onClose a callback function to handle closing the sidebar.
+ * @param term the term to be edited in single term edit mode
+ * @param column the column of the term to be edited in single term edit mode
+ * @param onSave a callback function to handle saving the edited term
+ * @param inBatchMode whether the sidebar is in batch edit mode; false by default
+ * @param vocabularyIri the IRI of the vocabulary in which the terms are being edited
+ * @param selectedTerms the terms selected for batch editing
+ * @param selectedTermIris the IRIs of the terms selected for batch editing. This is used to quickly filter the selected terms.
+ * @param onBatchSave a callback function to handle saving the batch edited terms
+ * @returns
+ */
 export const TermEditSidebar: React.FC<TermEditSidebarProps> = ({
   isOpen,
   language,
@@ -32,7 +53,8 @@ export const TermEditSidebar: React.FC<TermEditSidebarProps> = ({
   onSave,
   inBatchMode = false,
   vocabularyIri,
-  targetTerms,
+  selectedTerms,
+  selectedTermIris,
   onBatchSave,
 }) => {
   return (
@@ -45,7 +67,9 @@ export const TermEditSidebar: React.FC<TermEditSidebarProps> = ({
         {inBatchMode && onBatchSave ? (
           <BatchTermEditor
             vocabularyIri={vocabularyIri}
-            targetTerms={targetTerms || new Set()}
+            language={language}
+            selectedTerms={selectedTerms || []}
+            selectedTermIris={selectedTermIris || new Set<string>()}
             onSave={onBatchSave}
             onCancel={onClose}
           />
