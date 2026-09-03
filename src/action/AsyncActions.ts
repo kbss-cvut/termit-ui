b5381000d424bbf0664112d2278a1cfeda6dd357
Lukáš Kaňka <lukaskabc@gmail.com>
2026-09-03 10:43:21 +0200

    Revert "[Enhancement #822] Update endpoint used for quering flat list of terms"
    This reverts commit c67cba5a84a93a9ceeebcf204ad0f4d70878fd98.

--- src/action/AsyncActions.ts
+++ src/action/AsyncActions.ts
@@ -564,12 +564,8 @@ export function genericLoadTerms(
       const parentIri = VocabularyUtils.create(fetchOptions.optionID as string);
       url = `${getApiPrefix(getState())}/terms/${parentIri.fragment}/subterms`;
       target.namespace = parentIri.namespace;
-    } else if (!fetchOptions.searchString) {
-      if (fetchOptions.flatList) {
-        url += "/flat";
-      } else {
-        url += "/roots";
-      }
+    } else if (!fetchOptions.searchString && !fetchOptions.flatList) {
+      url += "/roots";
     }
     return Ajax.get(
       url,
