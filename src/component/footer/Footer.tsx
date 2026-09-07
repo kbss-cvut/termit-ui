227bf2a3ee0bc448f4828efe37131e07b3b1ef45
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2026-09-07 08:52:45 +0200

    [Ref] Rewrite Footer from a component class to a function component
    Removes i18n HOCs

--- src/component/footer/Footer.tsx
+++ src/component/footer/Footer.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { injectIntl } from "react-intl";
-import withI18n, { HasI18n } from "../hoc/withI18n";
+import { useI18n } from "../hook/useI18n";
 import "./Footer.scss";
 import Constants from "../../util/Constants";
 import { Col } from "reactstrap";
@@ -11,112 +10,92 @@ import classNames from "classnames";
 import ErrorLogViewer from "../audit/ErrorLogViewer";
 import NewsMd from "../dashboard/NewsMd";
 
-interface FooterProps extends HasI18n {
+interface FooterProps {
   transparent?: boolean;
   authenticated: boolean;
   sidebarExpanded: boolean;
 }
 
-interface FooterState {
-  showLog: boolean;
-  showNews: boolean;
-}
+const Footer: React.FC<FooterProps> = ({
+  transparent = false,
+  authenticated,
+  sidebarExpanded,
+}) => {
+  const { i18n } = useI18n();
+  const [showLog, setShowLog] = React.useState(false);
+  const [showNews, setShowNews] = React.useState(false);
 
-class Footer extends React.Component<FooterProps, FooterState> {
-  public static defaultProps = {
-    transparent: false,
-  };
+  const toggleLogViewer = () => setShowLog(!showLog);
+  const toggleNewsViewer = () => setShowNews((prevState) => !prevState);
 
-  public constructor(props: FooterProps) {
-    super(props);
-    this.state = {
-      showLog: false,
-      showNews: false,
-    };
-  }
-
-  private toggleLogViewer = () =>
-    this.setState({ showLog: !this.state.showLog });
-
-  private toggleNewsViewer = () =>
-    this.setState((prevState) => ({ showNews: !prevState.showNews }));
-
-  public render() {
-    const { i18n, transparent, authenticated, sidebarExpanded } = this.props;
-
-    return (
-      <footer
-        className={classNames("footer-row", { transparent: transparent })}
+  return (
+    <footer className={classNames("footer-row", { transparent: transparent })}>
+      <div
+        className={classNames("footer-left", {
+          "footer-left-expanded": sidebarExpanded,
+          "footer-left-collapsed": !sidebarExpanded,
+        })}
       >
-        <div
-          className={classNames("footer-left", {
-            "footer-left-expanded": sidebarExpanded,
-            "footer-left-collapsed": !sidebarExpanded,
-          })}
+        <FooterMenu fixed={authenticated} />
+      </div>
+      <Col className="px-sm-3 px-2">
+        <a
+          href="https://kbss.felk.cvut.cz"
+          target="_blank"
+          rel="noopener noreferrer"
+          title={i18n("footer.copyright")}
         >
-          <FooterMenu fixed={authenticated} />
-        </div>
-        <Col className="px-sm-3 px-2">
-          <a
-            href="https://kbss.felk.cvut.cz"
-            target="_blank"
-            rel="noopener noreferrer"
-            title={i18n("footer.copyright")}
-          >
-            &copy;&nbsp;{i18n("footer.copyright")}, {new Date().getFullYear()}
-          </a>
-        </Col>
-        <div>
-          <button
-            type="button"
-            onClick={this.toggleLogViewer}
-            className="log-viewer-toggle pl-xs-1 px-sm-3 px-2"
-            title={i18n("log-viewer.title")}
-            id="log-viewer-toggler"
-          >
-            <GoZap />
-          </button>
-          <FooterModalViewer
-            title="log-viewer.title"
-            show={this.state.showLog}
-            onClose={this.toggleLogViewer}
-          >
-            <ErrorLogViewer />
-          </FooterModalViewer>
-        </div>
-        <div>
-          <a
-            href={`${Constants.SERVER_URL}/swagger-ui/index.html`}
-            title={i18n("footer.apidocs.tooltip")}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {i18n("footer.apidocs")}
-          </a>
-        </div>
-        <div className="news-viewer-toggle px-sm-2 px-sm-3 px-1 text-right">
-          <button
-            type="button"
-            onClick={this.toggleNewsViewer}
-            id="news-toggler"
-            title={i18n("footer.version.tooltip")}
-          >
-            <span className="footer-version">
-              {i18n("footer.version")}&nbsp;
-            </span>
-            {Constants.VERSION}
-          </button>
-          <FooterModalViewer
-            title="news-viewer.title"
-            show={this.state.showNews}
-            onClose={this.toggleNewsViewer}
-          >
-            <NewsMd />
-          </FooterModalViewer>
-        </div>
-      </footer>
-    );
-  }
-}
+          &copy;&nbsp;{i18n("footer.copyright")}, {new Date().getFullYear()}
+        </a>
+      </Col>
+      <div>
+        <button
+          type="button"
+          onClick={toggleLogViewer}
+          className="log-viewer-toggle pl-xs-1 px-sm-3 px-2"
+          title={i18n("log-viewer.title")}
+          id="log-viewer-toggler"
+        >
+          <GoZap />
+        </button>
+        <FooterModalViewer
+          title="log-viewer.title"
+          show={showLog}
+          onClose={toggleLogViewer}
+        >
+          <ErrorLogViewer />
+        </FooterModalViewer>
+      </div>
+      <div>
+        <a
+          href={`${Constants.SERVER_URL}/swagger-ui/index.html`}
+          title={i18n("footer.apidocs.tooltip")}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {i18n("footer.apidocs")}
+        </a>
+      </div>
+      <div className="news-viewer-toggle px-sm-2 px-sm-3 px-1 text-right">
+        <button
+          type="button"
+          onClick={toggleNewsViewer}
+          id="news-toggler"
+          title={i18n("footer.version.tooltip")}
+        >
+          <span className="footer-version">{i18n("footer.version")}&nbsp;</span>
+          {Constants.VERSION}
+        </button>
+        <FooterModalViewer
+          title="news-viewer.title"
+          show={showNews}
+          onClose={toggleNewsViewer}
+        >
+          <NewsMd />
+        </FooterModalViewer>
+      </div>
+    </footer>
+  );
+};
 
-export default injectIntl(withI18n(Footer));
+export default Footer;
